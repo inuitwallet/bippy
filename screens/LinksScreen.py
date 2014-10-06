9080cd57f9453f032a50e0b9ea79f2ac0f234ca4
MJ2P <mj2p.sg@gmail.com>
2014-10-06 22:22:02 +0100

    added links screen to allow for advertising of cold storage.

---
+++ screens/LinksScreen.py
@@ -0,0 +1,51 @@
+from kivy.uix.screenmanager import Screen
+
+class LinksScreen(Screen):
+	"""
+		Display WoodWallets links to the user on request
+	"""
+
+	def __init__(self, BippyApp, **kwargs):
+		super(LinksScreen, self).__init__(**kwargs)
+		self.BippyApp = BippyApp
+
+		self.linkAccordion = self.ids.linkAccordion.__self__
+
+		self.public = self.ids.public.__self__
+		self.publicImage = self.ids.publicImage.__self__
+		self.publicUrl = self.ids.publicUrl.__self__
+
+		self.private = self.ids.private.__self__
+		self.privateImage = self.ids.privateImage.__self__
+		self.privateUrl = self.ids.privateUrl.__self__
+
+		self.double = self.ids.double.__self__
+		self.doubleImage = self.ids.doubleImage.__self__
+		self.doubleUrl = self.ids.doubleUrl.__self__
+
+	def reset_ui(self, dt):
+		"""
+			clear all text from the ui
+		"""
+		self.publicUrl.text = ''
+		self.privateUrl.text = ''
+		self.doubleUrl.text = ''
+		return
+
+	def get_public_ww(self, public):
+		"""
+			return the Woodwallets url for public key only
+		"""
+		return 'https://woodwallets.io/product/woodwallet-public-address?pub_addr=' + public + '&pub_coin=' + self.BippyApp.chosenCurrencyLongName + '&orig=bippy'
+
+	def get_private_ww(self, private):
+		"""
+			return the Woodwallets url for private key only
+		"""
+		return 'https://woodwallets.io/product/one-side-private-key-only?pvt_pvtkey=' + private + '&pvt_coin=' + self.BippyApp.chosenCurrencyLongName + '&orig=bippy'
+
+	def get_double_ww(self, public, private):
+		"""
+			return the double sided url for both private key and public address
+		"""
+		return 'https://woodwallets.io/product/woodwallet-private-key-and-public-address?dbl_addr=' + public + '&dbl_pvtkey=' + private + '&dbl_coin=' + self.BippyApp.chosenCurrencyLongName + '&orig=bippy'
