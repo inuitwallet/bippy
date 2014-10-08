ac74cea22ae386562a117d519ba47a9365b59569
MJ2P <mj2p.sg@gmail.com>
2014-10-08 09:56:54 +0100

    restructured init function and added config parsing

--- bippy.py
+++ bippy.py
@@ -6,12 +6,15 @@
 	Written by the creator of inuit (http://inuit-wallet.co.uk)
 """
 
-from kivy.config import Config
+from kivy.config import Config, ConfigParser
 Config.set('graphics', 'width', '1000')
 Config.set('graphics', 'height', '400')
 Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'fullscreen', '0')
+Config.set('input', 'mouse', 'mouse,disable_multitouch')
 
 from kivy.app import App
+from kivy.uix.settings import Settings
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
@@ -21,15 +24,12 @@ from kivy.uix.screenmanager import ScreenManager
 from kivy.animation import Animation
 from kivy.uix.actionbar import ActionBar
 from kivy.uix.screenmanager import SlideTransition
-from kivy.properties import StringProperty
 from kivy.uix.popup import Popup
-
 from kivy.lang import Builder
 
 import json
 
 #import the screens
-
 import screens.HomeScreen as HomeScreen
 import screens.ResultsScreen as ResultsScreen
 import screens.LinksScreen as LinksScreen
@@ -45,9 +45,11 @@ class TopActionBar(ActionBar):
 		The top Action Bar
 	"""
 
-	def __init__(self, **kwargs):
+	def __init__(self, BippyApp, **kwargs):
 		super(TopActionBar, self).__init__(**kwargs)
-		#self.infoButton = self.ids.topInfoButton.__self__
+
+		self.BippyApp = BippyApp
+
 		self.topActionView = self.ids.topActionView.__self__
 		self.topActionPrevious = self.ids.topActionPrevious.__self__
 		self.currencySpinner = self.ids.currencySpinner.__self__
@@ -61,67 +63,67 @@ class TopActionBar(ActionBar):
 		"""
 		#When we reset the function bar text as it fires this routine.
 		#catch that here
-		if screen == BippyApp.get_string('Action_Title'):
+		if screen == self.BippyApp.get_string('Action_Title'):
 			return
 
 		#set the state of the top bar according to the screen that has been requested
-		self.ids.topActionPrevious.with_previous = False if screen == BippyApp.get_string('Home_Screen') else True
+		self.topActionPrevious.with_previous = False if screen == self.BippyApp.get_string('Home_Screen') else True
 
 		#currencies are not important to Mnemonic encryption disable the currency spinner
-		if screen == BippyApp.get_string('Mnemonic_Seed_Screen'):
-			self.set_currency(BippyApp.get_string('No_Currency_Selected'))
-			self.ids.currencySpinner.disabled = True
+		if screen == self.BippyApp.get_string('Mnemonic_Seed_Screen'):
+			self.set_currency(self.BippyApp.get_string('No_Currency_Selected'))
+			self.currencySpinner.disabled = True
 		else:
 			self.set_currency(self.ids.currencySpinner.text)
-			self.ids.currencySpinner.disabled = False
+			self.currencySpinner.disabled = False
 
 		#the Action spinner always shows the choose action instruction
-		self.ids.actionSpinner.text = BippyApp.get_string('Action_Title')
+		self.actionSpinner.text = self.BippyApp.get_string('Action_Title')
 
 		#set the transition based on the chosen screen
-		BippyApp.mainScreenManager.transition = SlideTransition(direction='left') if screen != BippyApp.get_string('Home_Screen') else SlideTransition(direction='right')
+		self.BippyApp.mainScreenManager.transition = SlideTransition(direction='left') if screen != self.BippyApp.get_string('Home_Screen') else SlideTransition(direction='right')
 
 		#close the info window if it is open before choosing a new screen
-		if BippyApp.show_info is True:
-			BippyApp.toggle_info()
+		if self.BippyApp.show_info is True:
+			self.BippyApp.toggle_info()
 
 		#set the info to display the info for the chosen screen
-		BippyApp.set_info(screen)
+		self.BippyApp.set_info(screen)
 
 		#set the screen
-		BippyApp.mainScreenManager.current = screen
+		self.BippyApp.mainScreenManager.current = screen
 
 		#set the title based on the chosen screen
-		self.ids.topActionPrevious.title = BippyApp.get_string('Main_Title') + ' - ' + screen if screen != BippyApp.get_string('Home_Screen') else BippyApp.get_string('Main_Title')
+		self.topActionPrevious.title = self.BippyApp.get_string('Main_Title') + ' - ' + screen if screen != self.BippyApp.get_string('Home_Screen') else self.BippyApp.get_string('Main_Title')
 
 		#chosing the home screen resets the UI
-		if screen == BippyApp.get_string('Home_Screen'):
-			BippyApp.reset_ui()
+		if screen == self.BippyApp.get_string('Home_Screen'):
+			self.BippyApp.reset_ui()
 
 		#set which widget has focus based on the screen that is chosen
-		if screen == BippyApp.get_string('Private_Key_Screen'):
-			if BippyApp.privateKeyScreen.newKeyAccordionItem.collapse is True:
-				BippyApp.privateKeyScreen.privateKeyInputEK.focus = True
+		if screen == self.BippyApp.get_string('Private_Key_Screen'):
+			if self.BippyApp.privateKeyScreen.newKeyAccordionItem.collapse is True:
+				self.BippyApp.privateKeyScreen.privateKeyInputEK.focus = True
 			else:
-				BippyApp.privateKeyScreen.passfieldNK.focus = True
-		if screen == BippyApp.get_string('Mnemonic_Seed_Screen'):
-			BippyApp.mnemonicSeedScreen.seedfield.focus = True
-		if screen == BippyApp.get_string('Public_Key_Screen'):
-			BippyApp.publicKeyScreen.keyfield.focus = True
+				self.BippyApp.privateKeyScreen.passfieldNK.focus = True
+		if screen == self.BippyApp.get_string('Mnemonic_Seed_Screen'):
+			self.BippyApp.mnemonicSeedScreen.seedfield.focus = True
+		if screen == self.BippyApp.get_string('Public_Key_Screen'):
+			self.BippyApp.publicKeyScreen.keyfield.focus = True
 		return
 
 	def set_currency(self, currencyLongName):
 		"""
 			This is fired when a currency is chosen in the dropdown
 		"""
-		if currencyLongName == BippyApp.get_string('No_Currency_Selected'):
-			BippyApp.chosenCurrency = ''
-			self.ids.topActionPrevious.app_icon = 'data/logo/kivy-icon-32.png'
+		if currencyLongName == self.BippyApp.get_string('No_Currency_Selected'):
+			self.BippyApp.chosenCurrency = ''
+			self.topActionPrevious.app_icon = 'data/logo/kivy-icon-32.png'
 			return
-		BippyApp.chosenCurrency = BippyApp.get_currency_code(currencyLongName)
-		BippyApp.chosenCurrencyLongName = currencyLongName
-		self.ids.topActionPrevious.app_icon = 'res/icons/' + BippyApp.chosenCurrency.lower() + '.png'
-		self.ids.currencySpinner.text = currencyLongName
+		self.BippyApp.chosenCurrency = self.BippyApp.get_currency_code(currencyLongName)
+		self.BippyApp.chosenCurrencyLongName = currencyLongName
+		self.topActionPrevious.app_icon = 'res/icons/' + self.BippyApp.chosenCurrency.lower() + '.png'
+		self.currencySpinner.text = currencyLongName
 
 
 class BippyApp(App):
@@ -129,33 +131,39 @@ class BippyApp(App):
 		The application Class for Bippy
 	"""
 
-	passphrase = StringProperty(None)
-	chosenCurrency = StringProperty(None)
-	chosenCurrencyLongName = StringProperty(None)
-	#load the currencies.json file into memory
-	currencies = json.load(open('res/json/currencies.json', 'r'))
-	#This is the list of available currencies
-	currencyLongNamesList = ['Bitcoin', 'Litecoin', 'Dogecoin', 'Peercoin', 'Blackcoin', 'Vertcoin']
-
-	#Set the language and load the language file
-	language = 'english'
-	try:
-		lang = json.load(open('res/json/languages/' + language + '.json', 'r'))
-	except ValueError as e:
-		print('')
-		print('##################################################################')
-		print('')
-		print('There was an Error loading the ' + language + ' language file.')
-		print('')
-		print(str(e))
-		print('')
-		print('##################################################################')
-		raise SystemExit
-
 	def __init__(self, **kwargs):
 		super(BippyApp, self).__init__(**kwargs)
 		self.isPopup = False
 		self.show_info = False
+
+		self.use_kivy_settings = False
+
+		#load config file
+		#we don't display the settings panel, changes can be made manually to the ini file
+		self.config = ConfigParser()
+		self.config.read('bippy.ini')
+
+		self.chosenCurrency = ''
+		self.chosenCurrencyLongName = ''
+		#load the currencies.json file into memory
+		self.currencies = json.load(open('res/json/currencies.json', 'r'))
+		#This is the list of available currencies
+		self.currencyLongNamesList = ['Bitcoin', 'Litecoin', 'Dogecoin', 'Peercoin', 'Blackcoin', 'Vertcoin']
+
+		#Set the language and load the language file
+		self.language = self.config.get('Language', 'active_language')
+		try:
+			self.lang = json.load(open('res/json/languages/' + self.language + '.json', 'r'))
+		except ValueError as e:
+			print('')
+			print('##################################################################')
+			print('')
+			print('There was an Error loading the ' + self.language + ' language file.')
+			print('')
+			print(str(e))
+			print('')
+			print('##################################################################')
+			raise SystemExit
 		return
 
 	def build(self):
@@ -169,7 +177,7 @@ class BippyApp(App):
 		self.mainScreenManager = ScreenManager(transition=SlideTransition(direction='left'))
 
 		#Add the Action Bar
-		self.topActionBar = TopActionBar()
+		self.topActionBar = TopActionBar(self)
 		self.root.add_widget(self.topActionBar)
 
 		#Add the Scroll View For displaying Info
@@ -196,17 +204,17 @@ class BippyApp(App):
 		self.linksScreenWoodWallets = LinksScreen.LinksScreen(self, name=self.get_string('Links_Screen_Wood_Wallets'))
 
 		#Add the screens to the screen manager
-		BippyApp.mainScreenManager.add_widget(self.homeScreen)
-		BippyApp.mainScreenManager.add_widget(self.privateKeyScreen)
-		BippyApp.mainScreenManager.add_widget(self.intermediateCodeScreen)
-		BippyApp.mainScreenManager.add_widget(self.mnemonicSeedScreen)
-		BippyApp.mainScreenManager.add_widget(self.publicKeyScreen)
-		BippyApp.mainScreenManager.add_widget(self.vanityScreen)
-		BippyApp.mainScreenManager.add_widget(self.resultsScreen)
-		BippyApp.mainScreenManager.add_widget(self.linksScreenWoodWallets)
+		self.mainScreenManager.add_widget(self.homeScreen)
+		self.mainScreenManager.add_widget(self.privateKeyScreen)
+		self.mainScreenManager.add_widget(self.intermediateCodeScreen)
+		self.mainScreenManager.add_widget(self.mnemonicSeedScreen)
+		self.mainScreenManager.add_widget(self.publicKeyScreen)
+		self.mainScreenManager.add_widget(self.vanityScreen)
+		self.mainScreenManager.add_widget(self.resultsScreen)
+		self.mainScreenManager.add_widget(self.linksScreenWoodWallets)
 
 		#add the screenmanager to the root
-		self.root.add_widget(BippyApp.mainScreenManager)
+		self.root.add_widget(self.mainScreenManager)
 
 		return self.root
 
@@ -235,14 +243,14 @@ class BippyApp(App):
 			Read the info from the <language>.json file and set it as the info text
 		"""
 		screenName = None
-		for k, v in BippyApp.lang.iteritems():
+		for k, v in self.lang.iteritems():
 			if v == screen:
 				if 'Screen' not in k or 'Screen_Info' in k:
 					continue
 				screenName = k
 				break
 		if screenName is not None:
-			self.infoText.text = BippyApp.get_string(screenName + '_Info')
+			self.infoText.text = self.get_string(screenName + '_Info')
 
 	def toggle_info(self):
 		"""
@@ -286,7 +294,7 @@ class BippyApp(App):
 		self.isPopup = True
 		return
 
-	def close_popup(self, instance, value=False):
+	def close_popup(self, instance=False, value=False):
 		"""
 			Close the warning popup
 		"""
@@ -303,6 +311,13 @@ class BippyApp(App):
 		except (ValueError, KeyError):
 			return_string = 'Language Error'
 		return return_string
+
+	def open_settings(*args):
+		"""
+			override to disable F1 settings panel
+		"""
+		pass
+
 
 if __name__ == '__main__':
 	BippyApp = BippyApp()
